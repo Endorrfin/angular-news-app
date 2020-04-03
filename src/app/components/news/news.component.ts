f8ebdfd90cabd6e6d3361d968f8f8679a2bfa27d
Vasyl Krupka <krupka.ua@gmail.com>
2020-04-03 12:46:29 +0300

    select source in process

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -9,32 +9,61 @@ import {NewsApiService} from "../../services/news-api.service";
 
 export class NewsComponent implements OnInit {
   newsList;
-  currentCountry = 'ua'; // default country is Ukraine
-  currentCategory = 'health'; // default category is Health
+  currentCountry = 'us'; // default country is Ukraine
+  currentCategory = 'technology'; // default category is Health
+  currentSource = '';
   countryList = ['ua', 'us', 'pl', 'ca', 'gb', 'kz', 'ru']; // country list for country select
-  categoryList = ['business', 'entertainment', 'health', 'science', 'sports', 'technology']; // category list for category select
+  // countryList = ['Ukraine', 'USA', 'Pollen', 'Canada', 'Great Britain', 'Kazakhstan', 'Russia']; // country list for country select
+  categoryList = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology']; // category list for category select
+  sourceList = [];
 
 
   constructor(public news: NewsApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     // Get news
     this.news.getNewsByCountryAndCategory(this.currentCountry, this.currentCategory).subscribe(news => {
+      this.newsList = news['articles'];
+      // console.log(this.newsList)
+    });
+
+    // Get all sources
+    this.news.getSourcesByCountry(this.currentCountry, this.currentCategory).subscribe(sources => {
+      if(sources['sources'].length > 0) {
+        // console.log(sources)
+        this.sourceList = sources['sources'];
+        // console.log(this.sourceList);
+        this.currentSource = this.sourceList[0];
+      }
+    });
+
+    // console.log(this.news.getNewsByCountryAndCategoryAndSoure(1, 2, 3));
+    // console.log(this.news.getNewsByCountryAndCategoryAndSoure(1, null, 3));
+
+  }
+
+  // SELECT COUNTRY & CATEGORY
+  onChange() {
+    this.news.getNewsByCountryAndCategory(this.currentCountry, this.currentCategory).subscribe(news => {
+      this.newsList = news['articles'];
+    });
+  }
+
+  // onChangeSource() {
+  //   this.news.getNewsByCategoryCountrySource(this.currentCountry, this.currentCategory, this.currentSource).subscribe(news => {
+  //     console.log(news);
+  //     this.newsList = news['articles'];
+  //   });
+  // }
+
+
+  // SELECT SOURCE
+  onChangeSource() {
+    this.news.getNewsBySource(this.currentSource).subscribe(news => {
       console.log(news);
       this.newsList = news['articles'];
     });
   }
 
-  onChangeCountry() {
-    this.news.getNewsByCountryAndCategory(this.currentCountry, this.currentCategory).subscribe(news => {
-      this.newsList = news['articles'];
-    });
-  }
-
-  onChangeCategory() {
-    this.news.getNewsByCountryAndCategory(this.currentCountry, this.currentCategory).subscribe(news => {
-      this.newsList = news['articles'];
-    });
-  }
 
 }
